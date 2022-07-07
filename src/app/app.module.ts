55c3599c9be9d6153fa933bccd7f894fe1dd5146
skkkyyyeee <anoopru2002@gmail.com>
2022-07-08 02:45:34 +0530

    almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageHospitalComponent } from './frontpage-hospital/frontpage-hospital.component';
+import { SearchdeptHospitalComponent } from './searchdept-hospital/searchdept-hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageHospitalComponent
+    FrontpageHospitalComponent,
+    SearchdeptHospitalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchdept-hospital/searchdept-hospital.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchdept-hospital',
+  templateUrl: './searchdept-hospital.component.html',
+  styleUrls: ['./searchdept-hospital.component.css']
+})
+export class SearchdeptHospitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
