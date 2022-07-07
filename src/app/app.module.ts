f7e089b2ee48426dc44cdbb0a847957ce6cac115
skkkyyyeee <anoopru2002@gmail.com>
2022-07-08 02:29:02 +0530

    front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontpageHospitalComponent } from './frontpage-hospital/frontpage-hospital.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageHospitalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/frontpage-hospital/frontpage-hospital.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage-hospital',
+  templateUrl: './frontpage-hospital.component.html',
+  styleUrls: ['./frontpage-hospital.component.css']
+})
+export class FrontpageHospitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
